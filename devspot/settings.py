5a7357f44fa5b5e76f04424f39571e271c107174
Colton Sweeney <csweeney98@gmail.com>
2018-02-01 13:23:56 -0500

    setup virtualenv and fix up styling.

--- devspot/settings.py
+++ devspot/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = 'oqsp+$_xvsg$_=#7w0ffbj_#s_@0+$zg$snq0=(3@wui8x5=!d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -132,6 +132,6 @@ MEDIA_URL = 'media/'
 DEFAULT_FROM_EMAIL = 'testing@example.com'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'pizdetz'
-EMAIL_HOST_PASSWORD = HOST_PASSWORD
+EMAIL_HOST_PASSWORD = 'Sweeney98'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
